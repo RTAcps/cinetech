65b6caa952cb7679e7ed25424415a38682dd9385
Rodrigo Teixeira de Andrade <rodrigoandrade20@yahoo.com.br>
2024-03-15 09:24:45 -0300

    test: implement onResize to footer

--- src/app/core/component/footer/footer.component.spec.ts
+++ src/app/core/component/footer/footer.component.spec.ts
@@ -11,7 +11,7 @@ describe('FooterComponent', () => {
       imports: [FooterComponent]
     })
     .compileComponents();
-    
+
     fixture = TestBed.createComponent(FooterComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +19,26 @@ describe('FooterComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('#onResize', () => {
+    it('should set isMobileView to true if window.innerWidth <= 768', () => {
+      // Arrange
+      spyOnProperty(window, 'innerWidth', 'get').and.returnValue(768);
+      const mockEvent = { target: { innerWidth: 768 } };
+      // Act
+      component.onResize(mockEvent);
+      // Assert
+      expect(component.isMobileView).toBeTruthy();
+    });
+
+    it('should set isMobileView to false if window.innerWidth > 768', () => {
+      // Arrange
+      const mockEvent = { target: { innerWidth: 769 } };
+      // Act
+      component.onResize(mockEvent);
+      // Assert
+      expect(component.isMobileView).toBeFalsy();
+    });
   });
 });
--- src/app/core/component/header/header.component.spec.ts
+++ src/app/core/component/header/header.component.spec.ts
@@ -20,22 +20,24 @@ describe('HeaderComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should set isMobileView to true if window.innerWidth <= 768', () => {
-    // Arrange
-    spyOnProperty(window, 'innerWidth', 'get').and.returnValue(768);
-    const mockEvent = { target: { innerWidth: 768 } };
-    // Act
-    component.onResize(mockEvent);
-    // Assert
-    expect(component.isMobileView).toBeTruthy();
-  });
+  describe('#onResize', () => {
+    it('should set isMobileView to true if window.innerWidth <= 768', () => {
+      // Arrange
+      spyOnProperty(window, 'innerWidth', 'get').and.returnValue(768);
+      const mockEvent = { target: { innerWidth: 768 } };
+      // Act
+      component.onResize(mockEvent);
+      // Assert
+      expect(component.isMobileView).toBeTruthy();
+    });
 
-  it('should set isMobileView to false if window.innerWidth > 768', () => {
-    // Arrange
-    const mockEvent = { target: { innerWidth: 769 } };
-    // Act
-    component.onResize(mockEvent);
-    // Assert
-    expect(component.isMobileView).toBeFalsy();
+    it('should set isMobileView to false if window.innerWidth > 768', () => {
+      // Arrange
+      const mockEvent = { target: { innerWidth: 769 } };
+      // Act
+      component.onResize(mockEvent);
+      // Assert
+      expect(component.isMobileView).toBeFalsy();
+    });
   });
 });
