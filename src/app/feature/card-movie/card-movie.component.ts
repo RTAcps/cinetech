b36e1830cf9463b1aaf7a45a12bfcdb8a006309e
Rodrigo Teixeira de Andrade <rodrigoandrade20@yahoo.com.br>
2024-03-15 13:41:15 -0300

    feat: create card movie component

---
+++ src/app/feature/card-movie/card-movie.component.ts
@@ -0,0 +1,45 @@
+import { NgOptimizedImage, PRECONNECT_CHECK_BLOCKLIST } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
+import { NgbRatingModule } from '@ng-bootstrap/ng-bootstrap';
+import { Movie } from '../../shared/models/movie';
+
+@Component({
+  selector: 'app-card-movie',
+  standalone: true,
+  imports: [NgbRatingModule],
+  providers: [
+    {
+      provide: PRECONNECT_CHECK_BLOCKLIST,
+      useValue: 'https://m.media-amazon.com',
+    },
+  ],
+  templateUrl: './card-movie.component.html',
+  styleUrl: './card-movie.component.scss',
+})
+export class CardMovieComponent implements OnInit {
+  @Input() movieData!: Movie[];
+
+  public title!: string;
+  public year!: string;
+  public actors!: string;
+  public plot!: string;
+  public poster!: string;
+  public rating!: number;
+
+  ngOnInit(): void {
+    this.getInfo(this.movieData);
+  }
+
+  public ariaValueText(current: number, max: number) {
+    return `${current} out of ${max} stars`;
+  }
+
+  public getInfo(movie: any) {
+    this.title = movie?.Title;
+    this.year = movie?.Year;
+    this.actors = movie?.Actors;
+    this.plot = movie?.Plot;
+    this.poster = movie?.Poster;
+    this.rating = Number((movie?.imdbRating * 5) / 10);
+  }
+}
---
+++ src/app/shared/models/movie.ts
@@ -0,0 +1,31 @@
+export interface Movie {
+  Title: string;
+  Year: string;
+  Rated?: string;
+  Released?: string;
+  Runtime?: string;
+  Genre?: string;
+  Director?: string;
+  Writer?: string;
+  Actors: string;
+  Plot: string;
+  Language?: string;
+  Country?: string;
+  Awards?: string;
+  Poster: string;
+  Ratings?: [
+    { Source: string; Value: string },
+    { Source: string; Value: string },
+    { Source: string; Value: string }
+  ];
+  Metascore?: string;
+  imdbRating: string;
+  imdbVotes?: string;
+  imdbID?: string;
+  Type?: string;
+  DVD?: string;
+  BoxOffice?: string;
+  Production?: string;
+  Website?: string;
+  Responsew?: string;
+}
