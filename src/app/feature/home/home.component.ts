fa101a3c846f3ac8feee1690ff87e0c580ec7b2a
Rodrigo Teixeira de Andrade <rodrigoandrade20@yahoo.com.br>
2024-03-16 09:08:50 -0300

    refactor: rebuild the home so that it receives information from the other components

--- src/app/feature/home/home.component.ts
+++ src/app/feature/home/home.component.ts
@@ -1,97 +1,29 @@
-import { Component, OnInit } from '@angular/core';
-import Swal from 'sweetalert2';
+import { Component } from '@angular/core';
 import { FooterComponent } from '../../core/component/footer/footer.component';
 import { HeaderComponent } from '../../core/component/header/header.component';
+import { SearchMovieComponent } from '../../core/component/search-movie/search-movie.component';
 import { FilmeService } from '../../core/service/filme.service';
+import { Movie } from '../../shared/models/movie';
 import { CardMovieComponent } from '../card-movie/card-movie.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent, CardMovieComponent],
+  imports: [
+    HeaderComponent,
+    FooterComponent,
+    CardMovieComponent,
+    SearchMovieComponent,
+  ],
   providers: [FilmeService],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
   public title = 'CineTech';
+  public movieDetails!: Movie[];
 
-  constructor(private filmeService: FilmeService) {}
-
-  ngOnInit(): void {
-    this.buscarFilme();
-  }
-
-  public buscarFilme() {
-    this.filmeService.getFilme().subscribe({
-      next: (v) => console.log(v),
-      error: (e) => {
-        if (e.status === 400) {
-          const Toast = Swal.mixin({
-            toast: true,
-            position: 'top-end',
-            showConfirmButton: false,
-            timer: 3000,
-            timerProgressBar: true,
-            didOpen: (toast) => {
-              toast.onmouseenter = Swal.stopTimer;
-              toast.onmouseleave = Swal.resumeTimer;
-            },
-          });
-          Toast.fire({
-            icon: 'error',
-            title: 'Houve um erro inesperado!',
-          });
-        } else if (e.status === 401) {
-          const Toast = Swal.mixin({
-            toast: true,
-            position: 'top-end',
-            showConfirmButton: false,
-            timer: 3000,
-            timerProgressBar: true,
-            didOpen: (toast) => {
-              toast.onmouseenter = Swal.stopTimer;
-              toast.onmouseleave = Swal.resumeTimer;
-            },
-          });
-          Toast.fire({
-            icon: 'error',
-            title: 'Você não tem autorização para acessar!',
-          });
-        } else if (e.status === 404) {
-          const Toast = Swal.mixin({
-            toast: true,
-            position: 'top-end',
-            showConfirmButton: false,
-            timer: 3000,
-            timerProgressBar: true,
-            didOpen: (toast) => {
-              toast.onmouseenter = Swal.stopTimer;
-              toast.onmouseleave = Swal.resumeTimer;
-            },
-          });
-          Toast.fire({
-            icon: 'error',
-            title: 'Não foi possível encontrar o filme!',
-          });
-        } else if (e.status === 500) {
-          const Toast = Swal.mixin({
-            toast: true,
-            position: 'top-end',
-            showConfirmButton: false,
-            timer: 3000,
-            timerProgressBar: true,
-            didOpen: (toast) => {
-              toast.onmouseenter = Swal.stopTimer;
-              toast.onmouseleave = Swal.resumeTimer;
-            },
-          });
-          Toast.fire({
-            icon: 'error',
-            title: 'O servidor apresentou um problema!',
-          });
-        }
-      },
-    });
+  public updateMovies(movies: Movie[]): void {
+    this.movieDetails = movies;
   }
 }
