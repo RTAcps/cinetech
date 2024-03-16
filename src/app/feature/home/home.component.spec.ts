bce2ecb72eef90c5fc14d4870a95ebf2c38a9c11
Rodrigo Teixeira de Andrade <rodrigoandrade20@yahoo.com.br>
2024-03-16 13:25:32 -0300

    test: add reset movie and carousel to home

--- src/app/feature/home/home.component.spec.ts
+++ src/app/feature/home/home.component.spec.ts
@@ -4,6 +4,12 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { FilmeService } from '../../core/service/filme.service';
 import { Movie } from '../../shared/models/movie';
 import { HomeComponent } from './home.component';
+import { NgbSlideEvent, NgbSlideEventSource } from '@ng-bootstrap/ng-bootstrap';
+
+interface TempNgbSlideEvent {
+  paused: boolean;
+  source: NgbSlideEventSource;
+}
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -40,19 +46,118 @@ describe('HomeComponent', () => {
           Plot: 'Plot 1',
           Actors: 'Actor 1',
         },
-        {
-          Title: 'Movie 2',
-          Year: '2023',
-          imdbRating: '8.0',
-          Poster: 'poster2.jpg',
-          Plot: 'Plot 2',
-          Actors: 'Actor 2',
-        },
       ];
       // Act
       component.updateMovies(movies);
       // Assert
       expect(component.movieDetails).toEqual(movies);
+    });
+  });
+
+  describe('#resteMovies', () => {
+    it('should reset movieDetails to an empty array', () => {
+      // Arrange
+      component.movieDetails = [
+        {
+          Title: 'Movie 1',
+          Year: '2022',
+          imdbRating: '7.5',
+          Poster: 'poster1.jpg',
+          Plot: 'Plot 1',
+          Actors: 'Actor 1',
+        },
+      ];
+      // Act
+      component.resetMovies();
+      // Assert
+      expect(component.movieDetails).toEqual([]);
+    });
+  });
+
+  describe('#isFirstSlide', () => {
+    it('should return true if the movie is the first slide', () => {
+      // Arrange
+      const movie = 'Movie 1';
+      component.moviesPosters = ['Movie 1', 'Movie 2', 'Movie 3'];
+      // Act
+      const result = component.isFirstSlide(movie);
+      // Assert
+      expect(result).toBeTrue();
+    });
+
+    it('should return false if the movie is not the first slide', () => {
+      // Arrange
+      const movie = 'Movie 2';
+      component.moviesPosters = ['Movie 1', 'Movie 2', 'Movie 3'];
+      // Act
+      const result = component.isFirstSlide(movie);
+      // Assert
+      expect(result).toBeFalse();
+    });
+  });
+
+  describe('#togglePaused', () => {
+    it('should toggle paused state and call cycle() if paused is true', () => {
+      // Arrange
+      component.paused = true;
+      component.carousel = jasmine.createSpyObj('NgbCarousel', ['cycle']);
+      // Act
+      component.togglePaused();
+      // Assert
+      expect(component.paused).toBeFalse();
+      expect(component.carousel.cycle).toHaveBeenCalled();
+    });
+
+    it('should toggle paused state and call pause() if paused is false', () => {
+      // Arrange
+      component.paused = false;
+      component.carousel = jasmine.createSpyObj('NgbCarousel', ['pause']);
+      // Act
+      component.togglePaused();
+      // Assert
+      expect(component.paused).toBeTrue();
+      expect(component.carousel.pause).toHaveBeenCalled();
+    });
+  });
+
+  describe('#onSlide', () => {
+    it('should call togglePaused() when slide is not paused and source is indicator', () => {
+      // Arrange
+      const slideEvent: TempNgbSlideEvent = {
+        paused: false,
+        source: NgbSlideEventSource.INDICATOR,
+      };
+      spyOn(component, 'togglePaused');
+      // Act
+      component.onSlide(slideEvent as NgbSlideEvent);
+      // Assert
+      expect(component.togglePaused).toHaveBeenCalled();
+    });
+
+    it('should not call togglePaused() when slide is paused', () => {
+      // Arrange
+      const slideEvent: TempNgbSlideEvent = {
+        paused: true,
+        source: NgbSlideEventSource.INDICATOR,
+      };
+      spyOn(component, 'togglePaused');
+      // Act
+      component.onSlide(slideEvent as NgbSlideEvent);
+      // Assert
+      expect(component.togglePaused).not.toHaveBeenCalled();
+    });
+
+    it('should not call togglePaused() when source is not indicator', () => {
+      // Arrange
+      const slideEvent: TempNgbSlideEvent = {
+        paused: false,
+        source: NgbSlideEventSource.ARROW_LEFT,
+      };
+      spyOn(component, 'togglePaused');
+      // Act
+      component.onSlide(slideEvent as NgbSlideEvent);
+      // Assert
+      expect(component.togglePaused).not.toHaveBeenCalled();
     });
   });
 });
