137400cf4aca6571b808499d9fe676f307c91f49
Rodrigo Teixeira de Andrade <rodrigoandrade20@yahoo.com.br>
2024-03-17 10:38:51 -0300

    test: create favorite service

---
+++ src/app/core/service/favorite.service.spec.ts
@@ -0,0 +1,49 @@
+import { TestBed } from '@angular/core/testing';
+
+import { FavoriteService } from './favorite.service';
+import { take } from 'rxjs';
+
+describe('FavoriteService', () => {
+  let service: FavoriteService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(FavoriteService);
+    localStorage.clear();
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should toggle favorite status', () => {
+    service.toggleFavorite();
+    service.isFavorite$.pipe(take(1)).subscribe((isFavorite) => {
+      expect(isFavorite).toBeFalsy();
+    });
+    service.toggleFavorite();
+    service.isFavorite$.pipe(take(1)).subscribe((isFavorite) => {
+      expect(isFavorite).toBeTruthy();
+    });
+  });
+
+  it('should save favorite to localStorage', () => {
+    // Arrange
+    // Act
+    service.toggleFavorite();
+    // Assert
+    expect(localStorage.getItem(service['localStorageKey'])).toBeTruthy();
+  });
+
+  it('should handle localStorage not available', () => {
+    // Arrange
+    spyOn(window.localStorage, 'setItem').and.throwError(
+      'LocalStorage is not available'
+    );
+    spyOn(console, 'error');
+    // Act
+    service.toggleFavorite();
+    // Assert
+    expect(console.error).toHaveBeenCalledWith('LocalStorage is not available');
+  });
+});
