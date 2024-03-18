c36704e3bb65ad61e78d884c18669d0ac4857323
Rodrigo Teixeira de Andrade <rodrigoandrade20@yahoo.com.br>
2024-03-18 05:30:30 -0300

    test: add onResize to mobile

--- src/app/feature/home/home.component.spec.ts
+++ src/app/feature/home/home.component.spec.ts
@@ -160,4 +160,25 @@ describe('HomeComponent', () => {
       expect(component.togglePaused).not.toHaveBeenCalled();
     });
   });
+
+  describe('#onResize', () => {
+    it('should set isMobileView to true if window.innerWidth <= 768', () => {
+      // Arrange
+      spyOnProperty(window, 'innerWidth', 'get').and.returnValue(768);
+      const mockEvent = { target: { innerWidth: 768 } };
+      // Act
+      component.onResize(mockEvent);
+      // Assert
+      expect(component.isMobileView).toBeTruthy();
+    });
+
+    it('should set isMobileView to false if window.innerWidth > 768', () => {
+      // Arrange
+      const mockEvent = { target: { innerWidth: 769 } };
+      // Act
+      component.onResize(mockEvent);
+      // Assert
+      expect(component.isMobileView).toBeFalsy();
+    });
+  });
 });
